aa3181a521c4da6c99d67d2f2463803dc6f1a42a
ryanosaur <ryan.justin.taylor@gmail.com>
2018-02-21 10:44:24 -0500

    add package.json and Block class

---
+++ Block.js
@@ -0,0 +1,15 @@
+const sha256 = require('crypto-js/sha256')
+
+class Block {
+  constructor(index, timestamp, data, previousHash = '') {
+    this.index = index
+    this.timestamp = timestamp
+    this.data = data
+    this.previousHash = previousHash
+    this.hash = this.calculateHash()
+  }
+
+  calculateHash() {
+    return sha256(this.index + this.timestamp + JSON.stringify(this.data) + this.previousHash).toString()
+  }
+}
